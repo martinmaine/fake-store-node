43265c14a2dc7a0f4106ee16bcdb6766c0857622
martinmaine <105239643+martinmaine@users.noreply.github.com>
2023-03-08 00:17:09 -0300

    CORS

--- index.js
+++ index.js
@@ -3,20 +3,9 @@ const app = express();
 const port = 3000;
 const routerApi = require ('./routes')
 app.use (express.json());
-require('dotenv').config();
 
-const PORT = process.env.PORT || 8000;
-
-// Configuración de la base de datos
-/* const dbConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DB_NAME
-}
-
-app.use (dbConfig) */
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -24,6 +13,4 @@ app.get('/', (req, res) => {
 
 routerApi(app)
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(port)
