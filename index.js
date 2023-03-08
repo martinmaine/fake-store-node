efbb29e06a1e401a84b71d89a331794764704a47
martinmaine <martin.maine@hotmail.com>
2023-03-07 23:55:51 -0300

    initial commit

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 const port = 3000;
 const routerApi = require ('./routes')
 app.use (express.json());
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+// Configuración de la base de datos
+/* const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME
+}
+
+app.use (dbConfig) */
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -11,5 +25,5 @@ app.get('/', (req, res) => {
 routerApi(app)
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
